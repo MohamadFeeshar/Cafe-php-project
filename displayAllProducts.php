d71cb26c6c31a7c5792bd8af7e2babc93701a310
Reham97 <rehamhussein_fcis@yahoo.com>
2020-02-22 18:35:15 +0200

    update
    update design
    connect display all product page to database

--- displayAllProducts.php
+++ displayAllProducts.php
@@ -1,11 +1,16 @@
 <?php
+include ("DatabaseFunctions.php");
 
+$dbObject= new Database('localhost', 'test', 'test', 'cafedb');
+$myTest=$dbObject->getAllProducts();
+/*
 $myTest=[
     ["product"=>"product1","price"=>5,"image"=>"img1.jpg","statuse"=>"statuse1"],
     ["product"=>"product2","price"=>10,"image"=>"img2.jpg","statuse"=>"statuse2"],
     ["product"=>"product3","price"=>5,"image"=>"img3.jpg","statuse"=>"statuse3"],
     ["product"=>"product4","price"=>10,"image"=>"img4.jpg","statuse"=>"statuse4"]
     ];
+    */
 ?>
 
 <!DOCTYPE Html>
@@ -14,14 +19,15 @@ $myTest=[
 <?php  include("header.php"); ?>
 
 <section>
-<h1> All Products </h1>
+<h1 style="margin:10px 0;font-size: 3em;color:#ffffcc;"> All Products </h1>
 
-<a href="#">  <small style="float:right"> add product </small> </a>  
+<button class="allProductLink">add product</button>  
+</div>
 </section>
 
-<section>
+<section >
 
-
+<div style="overflow:auto;height:400px;overflow-x: hidden; margin-left:30%;margin-right:30%;">
 <table id="products">
     <tr>
     <th> Product </th>    
@@ -31,15 +37,45 @@ $myTest=[
     </tr>
 <?php foreach($myTest as $row)
 {    
-    echo "<tr><td>" . $row['product'] . "</td><td>" . $row['price'] ."</td><td>" . 
-    $row['image'] ."</td><td> <button style='background-color: #008CBA;'>" . $row['statuse'] .
-    "</button> <button style='background-color: #008CBA;'> Update </button> <button style='background-color: #008CBA;'> Delete  </button> </td></tr>"; 
-    
+    echo "<tr><td>" . $row['product_name'] . "</td><td>" . $row['price'] ."</td><td>" . 
+    $row['product_img'] ."</td><td> <button class='button availablebtn'> Available
+    </button> <button class='button updatebtn'> Update </button> <button class='button deletebtn'> Delete  </button> </td></tr>"; 
+  
 }
-echo "</table>"; 
+
+foreach($myTest as $row)
+{    
+    echo "<tr><td>" . $row['product_name'] . "</td><td>" . $row['price'] ."</td><td>" . 
+    $row['product_img'] ."</td><td> <button class='button availablebtn'> Available
+    </button> <button class='button updatebtn'> Update </button> <button class='button deletebtn'> Delete  </button> </td></tr>"; 
+  
+}
+foreach($myTest as $row)
+{    
+    echo "<tr><td>" . $row['product_name'] . "</td><td>" . $row['price'] ."</td><td>" . 
+    $row['product_img'] ."</td><td> <button class='button availablebtn'> Available
+    </button> <button class='button updatebtn'> Update </button> <button class='button deletebtn'> Delete  </button> </td></tr>"; 
+  
+}
+
+foreach($myTest as $row)
+{    
+    echo "<tr><td>" . $row['product_name'] . "</td><td>" . $row['price'] ."</td><td>" . 
+    $row['product_img'] ."</td><td> <button class='button availablebtn'> Available
+    </button> <button class='button updatebtn'> Update </button> <button class='button deletebtn'> Delete  </button> </td></tr>"; 
+  
+}
+foreach($myTest as $row)
+{    
+    echo "<tr><td>" . $row['product_name'] . "</td><td>" . $row['price'] ."</td><td>" . 
+    $row['product_img'] ."</td><td> <button class='button availablebtn'> Available
+    </button> <button class='button updatebtn'> Update </button> <button class='button deletebtn'> Delete  </button> </td></tr>"; 
+  
+} 
   
 ?>
-
+</table>
+</div>
 </section>
 
 
