529e6069aab90aa6e73616f735e883b1f97b6bf7
Rehab Ayman <rehabayman.fcis@gmail.com>
2020-02-26 13:25:53 +0200

    Add Ajax Request for order retrieval, Add getProductsOfOrder

--- admin/checksHelper.php
+++ admin/checksHelper.php
@@ -1,20 +1,22 @@
 <?php
 require_once('../databaseFunction/DatabaseFunctions.php');
 
-selectUser($_POST["user_id"]);
+if(isset($_POST["selectUser"]))
+{
+    selectUser($_POST["user_id"]);
+}
+if(isset($_POST["expandUserOrder"])) {
+    require '../configrationfile.php';
+    $db = new Database("127.0.0.1", $DBUserName, $DBPassword, "cafedb");
+    $usersWthTotal = $db->getOrdersOfUser($_POST["dateFrom"], $_POST["dateTo"], $_POST["user_id"]);
+    echo json_encode(['code'=>200, 'orders'=> $usersWthTotal]);
+}
+
 function selectUser($user_id)
 {
     require '../configrationfile.php';
     $db = new Database("127.0.0.1", $DBUserName, $DBPassword, "cafedb");
     $usersWthTotal = $db->getUsernameWthTotal($user_id);
-    echo json_encode(['code'=>200, 'users'=> $usersWthTotal]);
-}
-
-if(isset($_POST['expandUserOrder']))
-{
-    require '../configrationfile.php';
-    $db = new Database("127.0.0.1", $DBUserName, $DBPassword, "cafedb");
-    $usersWthTotal = $db->getspecificOrder($_POST["date-from"], $_POST["date-to"],$user_id);
     echo json_encode(['code'=>200, 'users'=> $usersWthTotal]);
 }
 
--- admin/js/checks.js
+++ admin/js/checks.js
@@ -1,18 +1,20 @@
 
-window.addEventListener("load", function(){
-    let dataObj = {user_id: -1};
+let dateFrom = document.getElementById("date-from");
+let dateTo = document.getElementById("date-to");
+
+window.addEventListener("load", function () {
+    let dataObj = { user_id: -1, selectUser: 1 };
     $.ajax({
         url: "checksHelper.php",
         type: "post",
         dataType: "json",
         data: dataObj,
-        success : function(data){
-            
-            if (data.code == "200"){
+        success: function (data) {
+
+            if (data.code == "200") {
                 let contentBody = document.getElementsByClassName("content")[0];
                 let child = contentBody.lastChild;
-                while (child)
-                {
+                while (child) {
                     contentBody.removeChild(child);
                     child = contentBody.lastChild;
                 }
@@ -26,56 +28,122 @@ window.addEventListener("load", function(){
                 tableRowHeader.appendChild(nameHeader);
                 tableRowHeader.appendChild(totalHeader);
                 table.appendChild(tableRowHeader);
+
                 let usersWthTotal = data.users;
-                
-                for (let i = 0; i < usersWthTotal.length; i++)
-                {
-                        let tableRow = document.createElement("tr");
-                    
-                        let nameData = document.createElement("td");
-                        nameData.innerHTML = usersWthTotal[i].user_name;
-                    
-                        let totalAmtData = document.createElement("td");
-                        totalAmtData.innerHTML = usersWthTotal[i].total_amount;
-                    
-                        tableRow.appendChild(nameData);
-                        tableRow.appendChild(totalAmtData);
-                        table.appendChild(tableRow);
-                    }
-                    
+
+                for (let i = 0; i < usersWthTotal.length; i++) {
+                    let tableRow = document.createElement("tr");
+
+                    let nameData = document.createElement("td");
+                    nameData.innerHTML = usersWthTotal[i].user_name;
+
+                    let totalAmtData = document.createElement("td");
+                    totalAmtData.innerHTML = usersWthTotal[i].total_amount;
+
+                    let userIdData = document.createElement("td");
+                    userIdData.setAttribute("style", "visibility:hidden;");
+                    userIdData.setAttribute("id", "userID");
+                    userIdData.innerHTML = usersWthTotal[i].user_id;
+
+                    tableRow.appendChild(nameData);
+                    tableRow.appendChild(totalAmtData);
+                    tableRow.appendChild(userIdData);
+                    table.appendChild(tableRow);
+
+                    tableRow.addEventListener("click", function (e) {
+                        let selectedUserID = e.path[2].rows[1].children[2].textContent; //selected user id
+
+                        if(dateFrom.value !== "" && dateTo.value !== "")
+                        {
+                            let obj = {user_id: selectedUserID, dateFrom: dateFrom.value, dateTo:dateTo.value, expandUserOrder: 1};
+                                                       
+                            $.ajax({
+                                url: "checksHelper.php",
+                                type: "post",
+                                dataType: "json",
+                                data: obj,
+                                success: function (data) {
+                                    if (data.code == "200")
+                                    {
+                                        // for(let i = 0; i < data.orders.length; i++){
+                                        //     //For Testing
+                                        //     console.log(data.orders[i].order_date);
+                                        //     console.log(data.orders[i].total_amount);
+                                            
+                                        // }
+                                        
+
+                                        /// HNA EL MAFROOD TE3REDY EL ORDER DATE ODAMO EL AMOUNT BETA3TO
+
+                                        // let table = document.createElement("table");
+                                        // // table.setAttribute("id", "data");
+                                        // let tableRowHeader = document.createElement("tr");
+                                        // let orderDateHeader = document.createElement("th");
+                                        // orderDateHeader.innerHTML = "Order Date";
+                                        // let totalHeader = document.createElement("th");
+                                        // totalHeader.innerHTML = "Amount";
+                                        // tableRowHeader.appendChild(nameHeader);
+                                        // tableRowHeader.appendChild(totalHeader);
+                                        // table.appendChild(tableRowHeader);
+
+                                        // for(let i = 0; i < data.orders.length; i++){
+                                        //     let tableRow = document.createElement("tr");
+
+                                        //     let orderDateData = document.createElement("td");
+                                        //     orderDateData.innerHTML = data.orders[i].order_date;
+                        
+                                        //     let totalAmtData = document.createElement("td");
+                                        //     totalAmtData.innerHTML = data.orders[i].total_amount;
+                        
+                                        //     tableRow.appendChild(nameData);
+                                        //     tableRow.appendChild(totalAmtData);
+                                        //     table.appendChild(tableRow);
+                                        // }
+                                        // contentBody.appendChild(table);
+                                        
+                                    }
+                                        
+                                }
+                            });
+                        }
+                        else
+                        {
+                            alert("You must specify date from and date to");
+                        }
+
+                    });
+                }
+
                 contentBody.appendChild(table);
             }
-            
-        } 
+
+        }
     });
 })
 
 let dropdownMenu = document.getElementById("users");
 
-dropdownMenu.addEventListener("change", function(){
+dropdownMenu.addEventListener("change", function () {
     let dataObj = {};
-    if(dropdownMenu.options[dropdownMenu.selectedIndex].value === "none")
-    {
-        dataObj = {user_id: -1};
+    if (dropdownMenu.options[dropdownMenu.selectedIndex].value === "none") {
+        dataObj = { user_id: -1, selectUser: 1 };
     }
-    else
-    {
-        dataObj = {user_id: parseInt(dropdownMenu.options[dropdownMenu.selectedIndex].value)};
+    else {
+        dataObj = { user_id: parseInt(dropdownMenu.options[dropdownMenu.selectedIndex].value), selectUser: 1 };
     }
-    
+
     $.ajax({
         url: "checksHelper.php",
         type: "post",
         dataType: "json",
         data: dataObj,
-        success : function(data){
-            
-            if (data.code == "200"){
-                
+        success: function (data) {
+
+            if (data.code == "200") {
+
                 let contentBody = document.getElementsByClassName("content")[0];
                 let child = contentBody.lastChild;
-                while (child)
-                {
+                while (child) {
                     contentBody.removeChild(child);
                     child = contentBody.lastChild;
                 }
@@ -89,27 +157,87 @@ dropdownMenu.addEventListener("change", function(){
                 tableRowHeader.appendChild(nameHeader);
                 tableRowHeader.appendChild(totalHeader);
                 table.appendChild(tableRowHeader);
-                
+
                 let usersWthTotal = data.users;
-                
-                for (let i = 0; i < usersWthTotal.length; i++)
-                {
-                        let tableRow = document.createElement("tr");
-                    
-                        let nameData = document.createElement("td");
-                        nameData.innerHTML = usersWthTotal[i].user_name;
-                    
-                        let totalAmtData = document.createElement("td");
-                        totalAmtData.innerHTML = usersWthTotal[i].total_amount;
-                    
-                        tableRow.appendChild(nameData);
-                        tableRow.appendChild(totalAmtData);
-                        table.appendChild(tableRow);
-                    }
-                    
+
+                for (let i = 0; i < usersWthTotal.length; i++) {
+                    let tableRow = document.createElement("tr");
+
+                    let nameData = document.createElement("td");
+                    nameData.innerHTML = usersWthTotal[i].user_name;
+
+                    let totalAmtData = document.createElement("td");
+                    totalAmtData.innerHTML = usersWthTotal[i].total_amount;
+
+                    let userIdData = document.createElement("td");
+                    userIdData.setAttribute("style", "visibility:hidden;");
+                    userIdData.setAttribute("id", "userID");
+                    userIdData.innerHTML = usersWthTotal[i].user_id;
+
+                    tableRow.appendChild(nameData);
+                    tableRow.appendChild(totalAmtData);
+                    tableRow.appendChild(userIdData);
+                    table.appendChild(tableRow);
+
+                    tableRow.addEventListener("click", function (e) {
+                        let selectedUserID = e.path[2].rows[1].children[2].textContent; //selected user id
+                        if(dateFrom.value !== "" && dateTo.value !== "")
+                        {
+                            let obj = {user_id: selectedUserID, dateFrom: dateFrom.value, dateTo:dateTo.value, expandUserOrder: 1};
+                                                       
+                            $.ajax({
+                                url: "checksHelper.php",
+                                type: "post",
+                                dataType: "json",
+                                data: obj,
+                                success: function (data) {
+                                    if (data.code == "200")
+                                    {
+
+                                        /// HNA EL MAFROOD TE3REDY EL ORDER DATE ODAMO EL AMOUNT BETA3TO
+
+                                        // let table = document.createElement("table");
+                                        // // table.setAttribute("id", "data");
+                                        // let tableRowHeader = document.createElement("tr");
+                                        // let orderDateHeader = document.createElement("th");
+                                        // orderDateHeader.innerHTML = "Order Date";
+                                        // let totalHeader = document.createElement("th");
+                                        // totalHeader.innerHTML = "Amount";
+                                        // tableRowHeader.appendChild(nameHeader);
+                                        // tableRowHeader.appendChild(totalHeader);
+                                        // table.appendChild(tableRowHeader);
+
+                                        // for(let i = 0; i < data.orders.length; i++){
+                                        //     let tableRow = document.createElement("tr");
+
+                                        //     let orderDateData = document.createElement("td");
+                                        //     orderDateData.innerHTML = data.orders[i].order_date;
+                        
+                                        //     let totalAmtData = document.createElement("td");
+                                        //     totalAmtData.innerHTML = data.orders[i].total_amount;
+                        
+                                        //     tableRow.appendChild(nameData);
+                                        //     tableRow.appendChild(totalAmtData);
+                                        //     table.appendChild(tableRow);
+                                        // }
+                                        // contentBody.appendChild(table);
+                                        
+                                    }
+                                        
+                                }
+                            });
+                        }
+                        else
+                        {
+                            alert("You must specify date from and date to");
+                        }
+
+                    });
+                }
+
                 contentBody.appendChild(table);
             }
-            
-        } 
+
+        }
     });
 });
