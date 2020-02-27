53cd2462a2d375c9ed179c69d29fa3eea4783536
MohamadFeeshar <mohamed.feeshar@gmail.com>
2020-02-27 13:14:13 +0200

    add (add user) form and database functions

--- admin/adduser.php
+++ admin/adduser.php
@@ -26,20 +26,20 @@ if (isset($_SESSION['login_user'])) {
     <section class="content" >
     <form action="addUserForm.php" method="post" enctype="multipart/form-data">
     <label for="username" class="fomrLable" style="font-size: 100%;">User Name</label>
-    <input type="text" name="username" id="username" class="formInput" style="padding: 4px 4px;margin: 2px 0;" ><br>
+    <input type="text" name="username" id="username" class="formInput" style="padding: 4px 4px;margin: 2px 0;" required><br>
     <label for="email" class="fomrLable" style="font-size: 100%;">Email:</label>
-    <input type="email" name="email" id="email" class="formInput" style="padding: 4px 4px;margin: 2px 0;" ><br>
+    <input type="email" name="email" id="email" class="formInput" style="padding: 4px 4px;margin: 2px 0;" required><br>
     <label for="password" class="fomrLable" style="font-size: 100%;">Password:</label>
-    <input type="password" name="password" id="password" class="formInput" style="padding: 4px 4px;margin: 2px 0;" ><br>
+    <input type="password" name="password" id="password" class="formInput" style="padding: 4px 4px;margin: 2px 0;" required><br>
     <label for="confirmPass" class="fomrLable" style="font-size: 100%;">Confirm passowrd:</label>
-    <input type="password" name="confirmpassword" id="confirmpassword" class="formInput" style="padding: 4px 4px;margin: 2px 0;" ><br>
-    <label for="RoomNo" class="fomrLable" style="font-size: 100%;">Room No.</label>
-    <input type="text" name="roomno" id="roomno" class="formInput" style="padding: 4px 4px;margin: 2px 0;" ><br>
+    <input type="password" name="confirmpassword" id="confirmpassword" class="formInput" style="padding: 4px 4px;margin: 2px 0;" required><br>
+    <label for="room" class="fomrLable" style="font-size: 100%;">Room No.</label>
+    <input type="text" name="room" id="room" class="formInput" style="padding: 4px 4px;margin: 2px 0;" required><br>
     <label for="ext" class="fomrLable" style="font-size: 100%;">Ext</label>
-    <input type="text" name="ext" id="ext" class="formInput" style="padding: 4px 4px;margin: 2px 0;" ><br>
+    <input type="text" name="ext" id="ext" class="formInput" style="padding: 4px 4px;margin: 2px 0;" required><br>
     <label for="pic" class="fomrLable" style="font-size: 100%;">Profile picture</label>
     <input type="file" id="profilePic" name="profilePic" class="formInput" style="padding: 4px 4px;margin: 2px 0;" ><br>
-    <input type="submit" value="Save" class ="save">
+    <input type="submit" value="submit" class ="save">
     <input type="reset" value="Reset" class="reset">
      </form>
 
